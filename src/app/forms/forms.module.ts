9685ee4fa6fb2790968afa78bba880e74f3627a8
omar.amribd <omar.amribd@goandev.net>
2020-08-23 09:13:53 +0100

    basic form

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { FormsRoutingModule } from './forms-routing.module';
 import { BasicFormComponent } from './containers/basic-form/basic-form.component';
+import { MaterialsModule } from '../materials/materials.module';
 
 
 @NgModule({
   declarations: [BasicFormComponent],
   imports: [
     CommonModule,
-    FormsRoutingModule
+    FormsRoutingModule,
+    MaterialsModule
   ]
 })
 export class FormsModule { }
